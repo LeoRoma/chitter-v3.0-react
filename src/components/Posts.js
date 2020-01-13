f0ead6a859c64bad7da63b3c1e9a43604866a322
LeoRoma <xiajtn@gmail.com>
2020-01-13 10:40:28 +0000

    api fetch

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
+// import axios from 'axios'
+
 
 class Posts extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: [],
+      isLoaded: false,
+    }
+  }
+
+  async componentDidMount() {
+    const url = 'https://chitter-backend-api.herokuapp.com/peeps';
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+  }
+
   render() {
     return (
       <div>
