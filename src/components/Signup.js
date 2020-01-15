4c08b12d03d86cbe0f9bb138df4a2681ce3621e3
LeoRoma <xiajtn@gmail.com>
2020-01-15 11:00:04 +0000

    button works

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -23,12 +23,16 @@ class SignUp extends Component {
   handlePasswordChange = event => {
     this.setState({ password: event.target.value })
   }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div class="container">
         <form onSubmit={this.handleSubmit} noValidate>
-
-
           {/* Username */}
           <TextField
             variant="outlined"
