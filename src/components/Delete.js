c67769951d3879189b413f8219e0758318edc46d
LeoRoma <xiajtn@gmail.com>
2020-01-28 12:48:12 +0000

    delete button works

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 
 class Delete extends Component {
+
+  handleDelete() {
+    console.log("hello")
+  }
+
   render() {
     return (
       <div>
-        Delete
+        <Button onClick={this.handleDelete.bind(this)}
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          Delete
+       </Button>
       </div>
     )
   }
